f196fa746d5122f81e7502d02d2340f42456b5f0
JanBricCodera <torridfyre@gmail.com>
2020-01-27 19:05:55 +0800

    added persistent navigation headers to shop and home page 2) added clothing logo in the header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import HomePage from "./pages/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import {Switch, Route} from 'react-router-dom';
-
+import Header from "./components/header/header.component";
 
 const HoodiesPage=()=>(
   <div>
@@ -16,6 +16,7 @@ const HoodiesPage=()=>(
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path = "/" component={HomePage}/>
         <Route path = "/hoodies" component={HoodiesPage}/> 
---
+++ src/components/header/header.component.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.styles.scss';
+import {Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '../../assets/exqst.svg';
+
+const header = () => (
+    <div className="header">
+        <Link className="logo-container" to="/">
+            <Logo classname="logo"/>
+        </Link>
+        <div className="options">
+            <Link className="option" to="/shop">SHOP</Link>
+            <Link className="option" to="/contact">CONTACT</Link>
+        </div>
+    </div>
+);
+
+
+export default header;
